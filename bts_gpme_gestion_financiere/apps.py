c61afad8d8907ce432b4fbdb89b33c89d3aff6e1
SAVRY Alice <165331699+AliceSavry@users.noreply.github.com>
2024-12-17 12:35:41 +0100

    Delete bts_gpme_gestion_financiere directory

--- bts_gpme_gestion_financiere/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BtsGpmeGestionFinanciereConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bts_gpme_gestion_financiere'
