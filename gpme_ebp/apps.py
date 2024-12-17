45b07db6c9162d4806a76cb98297ea674da1a948
SAVRY Alice <165331699+AliceSavry@users.noreply.github.com>
2024-12-17 12:42:30 +0100

    Add files via upload

---
+++ gpme_ebp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GpmeEbpConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gpme_ebp'
---
+++ gpme_gestion_risques/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GpmeGestionRisquesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gpme_gestion_risques'
